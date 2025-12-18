4a6cb5b637877b2489780bbbd9e7a6cb2b7e017f
anillakhaju12 <lakhajuanil12@gmail.com>
2025-12-18 22:34:22 +0545

    implement CORS in backend

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const bookRoute = require('./router/bookRouter');
+const cors = require('cors')
 require('./database/db')
 
 
 const PORT = process.env.PORT || 4000;
 
+
 const app = express();
+
+//middleware for cors
+app.use(cors({
+  origin : "http://localhost:5173"
+}))
 
 //middleware for the json data(it'll help express to understand the json data)
 app.use(express.json());
 
 //middleware for the route where "api" is concat with the bookRoute like if bookRoute contains /books then API will be localhost:5000/api/books
 app.use("/api/",bookRoute)
+
 
 app.listen(PORT,()=>{
   console.log( `Server has started on port http://localhost:${PORT}`)
